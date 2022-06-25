67db6b939de19b53fda126e142221fb38a178a99
Rhea <rheaazarraga@gmail.com>
2022-06-25 16:43:13 -0700

    import link and basic shopping icon, build out the navbar component as static to start

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href="/">BB-Tech Headphones</Link>
+      </p>
+
+      {/* button to open the cart */}
+      <button type='button' className='cart-icon' onclick=''>
+        <AiOutlineShopping />
+        <span className='cart-item-qty'>1</span>
+      </button>
+    </div>
   )
 }
 
