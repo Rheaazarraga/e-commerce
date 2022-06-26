1b13c59c174560f1dcb0cf2639ac5120960d566b
Rhea <rheaazarraga@gmail.com>
2022-06-25 18:28:40 -0700

    build out component structure, use getStaticProps function to pre-render the page with product details when a user clicks on a product, pass and destructure props

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,11 +1,40 @@
- // [] in file name indicates that the product slug will be dynamic
-
  import React from 'react';
+ import { client, urlFor } from '../../lib/client';
  
- const ProductDetails = () => {
+ const ProductDetails = ({ product, products }) => {
    return (
-     <div>Product</div>
+     <div>
+      <div className='product-detail-container'>
+        <div>
+          <div className='image-container'>
+            <img src='' />
+          </div>
+        </div>
+      </div>
+     </div>
    )
  }
+
+ export const getStaticProps = async({ params: { slug }}) => {
+  // get access to the slug that belongs to the product page clicked
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+
+  // fetch all the products
+  const productsQuery = '*[_type == "product"]';
+
+  // get the individual product
+  const product = await client.fetch(query);
+  const products = await client.fetch(productsQuery);
+
+  return {
+    props: { products, product }
+  }
+}
  
  export default ProductDetails
+
+  // [] in file name indicates that the product slug will be dynamic
+ // file based routing
+ // ----------
+ // getStaticProps is a built in Next.js function used when we want to pre-render the page at build time using the props returned
+ // the user can click on any of the product pages on the homepage, therefore, the product data should already be stored so everything instantly populates
