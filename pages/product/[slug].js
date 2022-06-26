a955fa747f6dbebf272a493a30d0a92a8199f949
Rhea <rheaazarraga@gmail.com>
2022-06-25 17:28:29 -0700

    create directory for product details page, add dynamic component to prevent a 404 when a product is clicked

---
+++ pages/product/[slug].js
@@ -0,0 +1,11 @@
+ // [] in file name indicates that the product slug will be dynamic
+
+ import React from 'react';
+ 
+ const ProductDetails = () => {
+   return (
+     <div>Product</div>
+   )
+ }
+ 
+ export default ProductDetails
