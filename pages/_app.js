e56f0989fe60bba14774c33c108232d3aa122cb6
Rhea <rheaazarraga@gmail.com>
2022-06-28 18:59:08 -0700

    make necessary imports, wrap the entire application with StateConext

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
 import '../styles/globals.css'
 import { Layout } from '../components';
+import { StateContext } from '../Context/StateContext';
+
+// small notification pop-up
+import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      {/* the component that the user is currently on */}
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        {/* the component that the user is currently on */}
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   )
 }
 
 export default MyApp
+
+
+/* wrapping the entire application with StateContext allows us to pass the 
+data from the StateContext to every single component inside it */
