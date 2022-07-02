f53c39c3e444bdcfd1d88021ec39c2bca5635687
Rhea <rheaazarraga@gmail.com>
2022-07-01 19:50:49 -0700

    import Cart component, and useStateContext states to pass dynamic data to the cart

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
 import { AiOutlineShopping } from 'react-icons/ai';
+import { Cart } from './';
+import { useStateContext } from '../context/StateContext';
 
 const NavBar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
   return (
     <div className='navbar-container'>
       <p className='logo'>
@@ -12,8 +15,10 @@ const NavBar = () => {
       {/* button to open the cart */}
       <button type='button' className='cart-icon' onclick=''>
         <AiOutlineShopping />
-        <span className='cart-item-qty'>1</span>
+        <span className='cart-item-qty'>{totalQuantities}</span>
       </button>
+
+      <Cart />
     </div>
   )
 }
