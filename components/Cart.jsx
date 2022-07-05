17b86640b088445aaf41048859ef455bf05bd8de
Rhea <rheaazarraga@gmail.com>
2022-07-04 17:16:10 -0700

    get all data needed from Context, start building out JSX for cart

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Link from 'next/link';
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
 import { TiDeleteOutline } from 'react-icons/ti';
@@ -8,8 +8,21 @@ import { useStateContext } from '../context/StateContext';
 import { urlFor } from '../lib/client';
 
 const Cart = () => {
+  // reference to cart
+  const cartRef = useRef();
+
+  const { totalPrice, totalQuantities, cartItems, setShowCart } = useStateContext();
+
   return (
-    <div>Cart</div>
+    <div className='cart-wrapper' ref={cartRef}>
+      <div className='cart-container'>
+        <button type='button' className='cart-heading' onClick={() => setShowCart(false)}>
+          <AiOutlineLeft />
+          <span className='heading'>Your Cart</span>
+          <span className='cart-num-items'>({totalQuantities} items)</span>
+        </button>
+      </div>
+    </div>
   )
 }
 
